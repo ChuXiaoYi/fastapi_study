0c13ff87cbb6b7cc4323c59dd4963eedbe65cadd
Xiaoyi Chu <chuxiaoyi@papayamobile.cn>
2020-08-28 16:57:34 +0800

    add: demo

---
+++ demo/demo9/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------
+#       @Time    : 2020/8/28 2:26 下午
+#       @Author  : cxy =.=
+#       @File    : main.py
+#       @Software: PyCharm
+#       @Desc    :
+# ---------------------------------------------
+from typing import Optional
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+        q: Optional[str] = Query(None, min_length=3, max_length=50, regex="^fixedquery$", title="Query string",
+                                 description="Query string for the items to search in the database that have a good match",
+                                 alias="item-query")):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
